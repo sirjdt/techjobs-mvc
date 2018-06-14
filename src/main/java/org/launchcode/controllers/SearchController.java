661e9959886c3133c49af72698d52e43ecded332
Jon Thompson <sirjdt@gmail.com>
2018-06-13 19:20:11 -0500

    working commit

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -23,5 +23,32 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String results(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        if (searchType.equals("all")) {
+            ArrayList<HashMap<String, String>> jobs = JobData.findAll();
+            model.addAttribute("columns", ListController.columnChoices);
+            model.addAttribute("jobs", jobs);
+            return "list-jobs";
+        } else {
+            ArrayList<HashMap<String, String>> items = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("columns", ListController.columnChoices);
+            model.addAttribute("jobs", items);
+            return "search";
+        }
+
+    }
+
 
 }
+
+/**Add another results handler method to SearchController, overloading the existing method.
+ The method should take in two parameters, specifying the type of search and the search term.
+ In order for the parameters to be properly passed in by Spring Boot, you'll need to name
+ them appropriately, based on the corresponding form field names. You'll also need to use
+ the correct annotations for the method and parameters. To configure the correct mapping
+ route, refer to the form action in search.html.
+
+ After looking up the search results via the JobData class, you'll need to pass them
+ into the search.html view via the model. You'll also need to pass
+ ListController.columnChoices to the view, as the existing search handler does.*/
